2d3895064f12ba9fb8f83db8c6f54c6f140ed0f4
Iswaryaparthi <153504644+Iswaryaparthi@users.noreply.github.com>
2024-04-22 12:53:35 +0530

    Update app_v1.py

--- app_v1.py
+++ app_v1.py
@@ -96,7 +96,7 @@ st.subheader(':green[Introduction]')
 st.markdown('''The objective of this project is to develop a machine learning model and deploy it as a user-friendly 
             web application that predicts the resale prices of flats in Singapore. 
             This predictive model will be based on historical data of resale flat transactions.''')
-st.subheader(':blue[Technolgies Used]')
+st.subheader(':blue[Technologies Used]')
 st.markdown(''' Python, Pandas, Numpy, SKlearn, Pickling, Streamlit, Render''')
 
 st.markdown(''':violet[Below UI will provides the user to select the values for each options and get the 
